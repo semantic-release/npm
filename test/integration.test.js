d9f3feb96da3b2aa29c2d192a65f90392cf1e8a1
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-11-06 01:47:39 -0500

    feat: Initial release

---
+++ test/integration.test.js
@@ -0,0 +1,184 @@
+import {promisify} from 'util';
+import {writeJson, readFile, appendFile} from 'fs-extra';
+import test from 'ava';
+import execa from 'execa';
+import {stub} from 'sinon';
+import tempy from 'tempy';
+import clearModule from 'clear-module';
+import SemanticReleaseError from '@semantic-release/error';
+import npmRegistry from './helpers/npm-registry';
+
+let processStderr;
+let processStdout;
+
+test.before(async t => {
+  // Start the local NPM registry
+  await npmRegistry.start();
+  // Disable npm logger during tests
+  processStderr = stub(process.stderr, 'write');
+  processStdout = stub(process.stdout, 'write');
+});
+
+test.beforeEach(async t => {
+  // Save the current process.env
+  t.context.env = Object.assign({}, process.env);
+  // Delete env paramaters that could have been set on the machine running the tests
+  delete process.env.NPM_TOKEN;
+  delete process.env.NPM_USERNAME;
+  delete process.env.NPM_PASSWORD;
+  delete process.env.NPM_EMAIL;
+  // Delete all `npm_config` environment variable set by CI as they take precedence over the `.npmrc` because the process that runs the tests is started before the `.npmrc` is created
+  for (let i = 0, keys = Object.keys(process.env); i < keys.length; i++) {
+    if (keys[i].startsWith('npm_config')) {
+      delete process.env[keys[i]];
+    }
+  }
+  // Save the current working diretory
+  t.context.cwd = process.cwd();
+  // Change current working directory to a temp directory
+  process.chdir(tempy.directory());
+  // Prevent to use `.npmrc` from the home directory in case there is a valid token set there
+  process.env.HOME = process.cwd();
+  process.env.USERPROFILE = process.cwd();
+  // Clear npm cache to refresh the module state
+  clearModule('../index');
+  t.context.m = require('../index');
+  // Stub the logger
+  t.context.log = stub();
+  t.context.logger = {log: t.context.log};
+});
+
+test.afterEach.always(async t => {
+  // Clear `rc` from the npm cache as it cache the relative path of .npmrc files, preventing to load a new file after changing current working directory
+  clearModule('rc');
+  // Restore process.env
+  process.env = Object.assign({}, t.context.env);
+  // Restore the current working directory
+  process.chdir(t.context.cwd);
+});
+
+test.after.always(async t => {
+  // Stop the local NPM registry
+  await npmRegistry.stop();
+  // Restore stdout and stderr
+  processStderr.restore();
+  processStdout.restore();
+});
+
+test.serial('Throws error if NPM token is invalid', async t => {
+  process.env.NPM_TOKEN = 'wrong_token';
+  const error = await t.throws(
+    promisify(t.context.m.verifyConditions)({}, {pkg: {name: 'invalid-token'}, logger: t.context.logger})
+  );
+
+  t.true(error instanceof SemanticReleaseError);
+  t.is(error.code, 'EINVALIDNPMTOKEN');
+  t.is(error.message, 'Invalid npm token.');
+
+  const npmrc = (await readFile('.npmrc')).toString();
+  t.regex(npmrc, /:_authToken/);
+});
+
+test.serial('Verify npm auth and package', async t => {
+  Object.assign(process.env, npmRegistry.authEnv);
+  const pkg = {name: 'valid-token', publishConfig: {registry: npmRegistry.url}};
+  await t.notThrows(promisify(t.context.m.verifyConditions)({}, {pkg, logger: t.context.logger}));
+
+  const npmrc = (await readFile('.npmrc')).toString();
+  t.regex(npmrc, /_auth =/);
+  t.regex(npmrc, /email =/);
+});
+
+test.serial('Return nothing if no version if published', async t => {
+  Object.assign(process.env, npmRegistry.authEnv);
+  const pkg = {name: 'not-published', publishConfig: {registry: npmRegistry.url}};
+  const nextRelease = await promisify(t.context.m.getLastRelease)({}, {pkg, logger: t.context.logger});
+
+  t.deepEqual(nextRelease, {});
+});
+
+test.serial('Return last version published', async t => {
+  Object.assign(process.env, npmRegistry.authEnv);
+  const pkg = {name: 'published', version: '1.0.0', publishConfig: {registry: npmRegistry.url}};
+  await writeJson('./package.json', pkg);
+
+  await appendFile(
+    './.npmrc',
+    `_auth = ${Buffer.from(`${process.env.NPM_USERNAME}:${process.env.NPM_PASSWORD}`, 'utf8').toString(
+      'base64'
+    )}\nemail = ${process.env.NPM_EMAIL}`
+  );
+
+  await execa('npm', ['publish']);
+
+  const nextRelease = await promisify(t.context.m.getLastRelease)({}, {pkg, logger: t.context.logger});
+  t.is(nextRelease.version, '1.0.0');
+});
+
+test.serial('Return last version published on a dist-tag', async t => {
+  Object.assign(process.env, npmRegistry.authEnv);
+  const pkg = {name: 'published-next', version: '1.0.0', publishConfig: {registry: npmRegistry.url, tag: 'next'}};
+  await writeJson('./package.json', pkg);
+
+  await appendFile(
+    './.npmrc',
+    `_auth = ${Buffer.from(`${process.env.NPM_USERNAME}:${process.env.NPM_PASSWORD}`, 'utf8').toString(
+      'base64'
+    )}\nemail = ${process.env.NPM_EMAIL}`
+  );
+
+  // Publish version 1.0.0 on latest and next
+  await execa('npm', ['publish', '--tag=next']);
+  pkg.version = '1.1.0';
+  await writeJson('./package.json', pkg);
+  // Publish version 1.1.0 on next
+  await execa('npm', ['publish', '--tag=next']);
+
+  const nextRelease = await promisify(t.context.m.getLastRelease)({}, {pkg, logger: t.context.logger});
+  t.is(nextRelease.version, '1.1.0');
+});
+
+test.serial('Return nothing for an unpublished package', async t => {
+  Object.assign(process.env, npmRegistry.authEnv);
+  const pkg = {name: 'unpublished', version: '1.0.0', publishConfig: {registry: npmRegistry.url}};
+  await writeJson('./package.json', pkg);
+
+  await appendFile(
+    './.npmrc',
+    `_auth = ${Buffer.from(`${process.env.NPM_USERNAME}:${process.env.NPM_PASSWORD}`, 'utf8').toString(
+      'base64'
+    )}\nemail = ${process.env.NPM_EMAIL}`
+  );
+
+  await execa('npm', ['publish']);
+  await execa('npm', ['unpublish', 'unpublished', '--force']);
+
+  const nextRelease = await promisify(t.context.m.getLastRelease)({}, {pkg, logger: t.context.logger});
+  t.deepEqual(nextRelease, {});
+});
+
+test.serial('Publish a package', async t => {
+  Object.assign(process.env, npmRegistry.authEnv);
+  const pkg = {name: 'publish', version: '1.0.0', publishConfig: {registry: npmRegistry.url}};
+  await writeJson('./package.json', pkg);
+
+  await promisify(t.context.m.publish)({}, {pkg, logger: t.context.logger, nextRelease: {version: '1.0.0'}});
+
+  t.is((await execa('npm', ['view', 'publish', 'version'])).stdout, '1.0.0');
+});
+
+test.serial('Verify token and set up auth only on the fist call', async t => {
+  Object.assign(process.env, npmRegistry.authEnv);
+  const pkg = {name: 'test-module', version: '0.0.0-dev', publishConfig: {registry: npmRegistry.url}};
+  await writeJson('./package.json', pkg);
+
+  await t.notThrows(promisify(t.context.m.verifyConditions)({}, {pkg, logger: t.context.logger}));
+
+  let nextRelease = await promisify(t.context.m.getLastRelease)({}, {pkg, logger: t.context.logger});
+  t.deepEqual(nextRelease, {});
+
+  await promisify(t.context.m.publish)({}, {pkg, logger: t.context.logger, nextRelease: {version: '1.0.0'}});
+
+  nextRelease = await promisify(t.context.m.getLastRelease)({}, {pkg, logger: t.context.logger});
+  t.is(nextRelease.version, '1.0.0');
+});
---
+++ test/update-package-version.test.js
@@ -0,0 +1,47 @@
+import {writeJson, readJson} from 'fs-extra';
+import test from 'ava';
+import tempy from 'tempy';
+import execa from 'execa';
+import {stub} from 'sinon';
+import updatePackageVersion from '../lib/update-package-version';
+
+test.beforeEach(async t => {
+  t.context.cwd = process.cwd();
+  process.chdir(tempy.directory());
+
+  t.context.log = stub();
+  t.context.logger = {log: t.context.log};
+});
+
+test.afterEach.always(t => {
+  // Restore the current working directory
+  process.chdir(t.context.cwd);
+});
+
+test.serial('Updade package.json', async t => {
+  // Create package.json in repository root
+  await writeJson('./package.json', {version: '0.0.0-dev'});
+
+  await updatePackageVersion('1.0.0', t.context.logger);
+
+  // Verify package.json has been updated
+  t.is((await readJson('./package.json')).version, '1.0.0');
+  // Verify the logger has been called with the version updated
+  t.true(t.context.log.calledWithMatch(/package.json/, '1.0.0'));
+});
+
+test.serial('Updade package.json and npm-shrinkwrap.json', async t => {
+  // Create package.json in repository root
+  await writeJson('./package.json', {version: '0.0.0-dev'});
+  // Create a npm-shrinkwrap.json file
+  await execa('npm', ['shrinkwrap']);
+
+  await updatePackageVersion('1.0.0', t.context.logger);
+
+  // Verify package.json and npm-shrinkwrap.json have been updated
+  t.is((await readJson('./package.json')).version, '1.0.0');
+  t.is((await readJson('./npm-shrinkwrap.json')).version, '1.0.0');
+  // Verify the logger has been called with the version updated
+  t.true(t.context.log.calledWithMatch(/package.json/, '1.0.0'));
+  t.true(t.context.log.calledWithMatch(/npm-shrinkwrap.json/, '1.0.0'));
+});
