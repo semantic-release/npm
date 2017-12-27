a560839c64fb128d730063324ff76838c5db75ca
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-12-27 14:52:56 -0500

    fix: add message if no gitHead found

--- test/get-last-release.test.js
+++ test/get-last-release.test.js
@@ -2,9 +2,9 @@ import {appendFile} from 'fs-extra';
 import test from 'ava';
 import nock from 'nock';
 import {stub} from 'sinon';
-import tempy from 'tempy';
 import lastRelease from '../lib/get-last-release';
-import {registry, mock, availableModule, unpublishedModule} from './helpers/mock-registry';
+import {gitRepo, gitCommit, gitTagVersion} from './helpers/git-utils';
+import {registry, mock, availableModule, unpublishedModule, missingGitHead} from './helpers/mock-registry';
 
 // Save the current process.env
 const envBackup = Object.assign({}, process.env);
@@ -14,7 +14,7 @@ const cwd = process.cwd();
 stub(process.stdout, 'write');
 stub(process.stderr, 'write');
 
-test.beforeEach(t => {
+test.beforeEach(async t => {
   process.env.NPM_TOKEN = 'npm_token';
   // Delete all `npm_config` environment variable set by CI as they take precedence over the `.npmrc` because the process that runs the tests is started before the `.npmrc` is created
   for (let i = 0, keys = Object.keys(process.env); i < keys.length; i++) {
@@ -22,8 +22,8 @@ test.beforeEach(t => {
       delete process.env[keys[i]];
     }
   }
-  // Change current working directory to a temp directory
-  process.chdir(tempy.directory());
+  // Create a git repository, set the current working directory at the root of the repo
+  await gitRepo();
   // Stub the logger
   t.context.log = stub();
   t.context.logger = {log: t.context.log};
@@ -41,7 +41,7 @@ test.afterEach.always(() => {
 test.serial('Get release from package name', async t => {
   const name = 'available';
   const registryMock = mock(name).reply(200, availableModule);
-  const release = await lastRelease({name, publishConfig: {registry}}, t.context.logger);
+  const release = await lastRelease({name, publishConfig: {registry}}, 'master', t.context.logger);
 
   t.is(release.version, '1.33.7');
   t.is(release.gitHead, 'HEAD');
@@ -51,7 +51,7 @@ test.serial('Get release from package name', async t => {
 test.serial("Get release from a tagged package's name", async t => {
   const name = 'tagged';
   const registryMock = mock(name).reply(200, availableModule);
-  const release = await lastRelease({name, publishConfig: {registry, tag: 'foo'}}, t.context.logger);
+  const release = await lastRelease({name, publishConfig: {registry, tag: 'foo'}}, 'master', t.context.logger);
 
   t.is(release.version, '0.8.15');
   t.is(release.gitHead, 'bar');
@@ -61,7 +61,7 @@ test.serial("Get release from a tagged package's name", async t => {
 test.serial('Get release from the latest fallback tag', async t => {
   const name = 'tagged';
   const registryMock = mock(name).reply(200, availableModule);
-  const release = await lastRelease({name, publishConfig: {registry, tag: 'bar'}}, t.context.logger);
+  const release = await lastRelease({name, publishConfig: {registry, tag: 'bar'}}, 'master', t.context.logger);
 
   t.is(release.version, '1.33.7');
   t.is(release.gitHead, 'HEAD');
@@ -72,7 +72,7 @@ test.serial('Get release from scoped package name', async t => {
   const name = '@scoped/available';
   const registryMock = mock(name).reply(200, availableModule);
 
-  const release = await lastRelease({name, publishConfig: {registry}}, t.context.logger);
+  const release = await lastRelease({name, publishConfig: {registry}}, 'master', t.context.logger);
   t.is(release.version, '1.33.7');
   t.is(release.gitHead, 'HEAD');
   t.true(registryMock.isDone());
@@ -81,7 +81,7 @@ test.serial('Get release from scoped package name', async t => {
 test.serial('Get nothing from completely unpublished package name', async t => {
   const name = 'completely-unpublished';
   const registryMock = mock(name).reply(200, unpublishedModule);
-  const release = await lastRelease({name, publishConfig: {registry}}, t.context.logger);
+  const release = await lastRelease({name, publishConfig: {registry}}, 'master', t.context.logger);
 
   t.falsy(release);
   t.true(registryMock.isDone());
@@ -90,7 +90,7 @@ test.serial('Get nothing from completely unpublished package name', async t => {
 test.serial('Get nothing from not yet published package name (unavailable)', async t => {
   const name = 'unavailable';
   const registryMock = mock(name).reply(404, {});
-  const release = await lastRelease({name, publishConfig: {registry}}, t.context.logger);
+  const release = await lastRelease({name, publishConfig: {registry}}, 'master', t.context.logger);
 
   t.falsy(release);
   t.true(registryMock.isDone());
@@ -99,7 +99,7 @@ test.serial('Get nothing from not yet published package name (unavailable)', asy
 test.serial('Get nothing from not yet published package name (unavailable w/o response body)', async t => {
   const name = 'unavailable-no-body';
   const registryMock = mock(name).reply(404);
-  const release = await lastRelease({name, publishConfig: {registry}}, t.context.logger);
+  const release = await lastRelease({name, publishConfig: {registry}}, 'master', t.context.logger);
 
   t.falsy(release);
   t.true(registryMock.isDone());
@@ -109,7 +109,7 @@ test.serial('Get registry from ".npmrc"', async t => {
   const name = 'available';
   const registryMock = mock(name).reply(200, availableModule);
   await appendFile('./.npmrc', `registry = ${registry}`);
-  const release = await lastRelease({name}, t.context.logger);
+  const release = await lastRelease({name}, 'master', t.context.logger);
 
   t.is(release.version, '1.33.7');
   t.is(release.gitHead, 'HEAD');
@@ -122,7 +122,7 @@ test.serial('Get nothing from not yet published package name (unavailable w/o st
     .times(3)
     .replyWithError({message: 'not found', statusCode: 500, code: 'E500'});
   await appendFile('./.npmrc', 'fetch-retry-factor = 1\nfetch-retry-mintimeout = 1\nfetch-retry-maxtimeout = 1');
-  const release = await lastRelease({name, publishConfig: {registry}}, t.context.logger);
+  const release = await lastRelease({name, publishConfig: {registry}}, 'master', t.context.logger);
 
   t.falsy(release);
   t.true(registryMock.isDone());
@@ -135,7 +135,7 @@ test.serial('Send bearer authorization using NPM_TOKEN', async t => {
     availableModule
   );
   await appendFile('./.npmrc', `registry = ${registry}\nalways-auth = true`);
-  const release = await lastRelease({name}, t.context.logger);
+  const release = await lastRelease({name}, 'master', t.context.logger);
 
   t.is(release.version, '1.33.7');
   t.is(release.gitHead, 'HEAD');
@@ -151,10 +151,33 @@ test.serial('Uses basic auth when always-auth=true in ".npmrc"', async t => {
     .basicAuth({user: 'username', pass: 'password'})
     .reply(200, availableModule);
   await appendFile('./.npmrc', `registry = ${registry}\nalways-auth = true`);
-  const release = await lastRelease({name}, t.context.logger);
+  const release = await lastRelease({name}, 'master', t.context.logger);
 
   t.is(release.version, '1.33.7');
   t.is(release.gitHead, 'HEAD');
+  t.true(registryMock.isDone());
+});
+
+test.serial('Retrieve "gitHead" from git tag', async t => {
+  const commit = await gitCommit('First');
+  await gitTagVersion('v1.33.7');
+
+  const name = 'missing-githead';
+  const registryMock = mock(name).reply(200, missingGitHead);
+  const release = await lastRelease({name, publishConfig: {registry}}, 'master', t.context.logger);
+
+  t.is(release.version, '1.33.7');
+  t.is(release.gitHead.substring(0, 7), commit.hash);
+  t.true(registryMock.isDone());
+});
+
+test.serial('Throws SemanticReleaseError if the gitHead is missing', async t => {
+  const name = 'missing-githead';
+  const registryMock = mock(name).reply(200, missingGitHead);
+  const error = await t.throws(lastRelease({name, publishConfig: {registry}}, 'master', t.context.logger));
+
+  t.is(error.name, 'SemanticReleaseError');
+  t.is(error.code, 'ENOGITHEAD');
   t.true(registryMock.isDone());
 });
 
@@ -164,7 +187,10 @@ test.serial('Throws error on server error', async t => {
     .times(3)
     .reply(500);
   await appendFile('./.npmrc', 'fetch-retry-factor = 1\nfetch-retry-mintimeout = 1\nfetch-retry-maxtimeout = 1');
-  await t.throws(lastRelease({name, publishConfig: {registry}}, t.context.logger), /500 Internal Server Error/);
+  await t.throws(
+    lastRelease({name, publishConfig: {registry}}, 'master', t.context.logger),
+    /500 Internal Server Error/
+  );
 
   t.true(registryMock.isDone());
 });
@@ -172,7 +198,11 @@ test.serial('Throws error on server error', async t => {
 test.serial('Handle missing trailing slash on registry URL', async t => {
   const name = 'available';
   const registryMock = mock(name).reply(200, availableModule);
-  const release = await lastRelease({name, publishConfig: {registry: 'http://registry.npmjs.org'}}, t.context.logger);
+  const release = await lastRelease(
+    {name, publishConfig: {registry: 'http://registry.npmjs.org'}},
+    'master',
+    t.context.logger
+  );
 
   t.is(release.version, '1.33.7');
   t.is(release.gitHead, 'HEAD');
