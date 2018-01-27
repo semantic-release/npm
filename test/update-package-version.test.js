da718a358714e48a8307618a56a2593e59fa0339
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-01-27 01:10:32 -0500

    fix: format `package.json` and `npm-shrinkwrap.json` as `npm`

--- test/update-package-version.test.js
+++ test/update-package-version.test.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import {outputFile, readFile} from 'fs-extra';
+import {outputJson, readJson} from 'fs-extra';
 import tempy from 'tempy';
 import execa from 'execa';
 import {stub} from 'sinon';
@@ -21,72 +21,48 @@ test.afterEach.always(() => {
 });
 
 test.serial('Updade package.json', async t => {
-  const pkg = `{
-  "name": "test",
-  "description": "pacakage description",
-  "version":
-       "0.0.0-dev"   ,
-  "arr": [
-    1,
-        2, 3
-      ]
-}
-`;
-
   // Create package.json in repository root
-  await outputFile('./package.json', pkg);
+  await outputJson('./package.json', {version: '0.0.0-dev'});
 
   await updatePackageVersion('1.0.0', '.', t.context.logger);
 
   // Verify package.json has been updated
-  t.is((await readFile('./package.json')).toString(), pkg.replace('0.0.0-dev', '1.0.0'));
+  t.is((await readJson('./package.json')).version, '1.0.0');
 
   // Verify the logger has been called with the version updated
   t.deepEqual(t.context.log.args[0], ['Wrote version %s to %s', '1.0.0', 'package.json']);
 });
 
 test.serial('Updade package.json and npm-shrinkwrap.json', async t => {
-  const pkg = `{
-    "name": "test"
-    ,    "description":
-"pacakage description",
-
-    "version":      "0.0.0-dev"
-  }`;
   // Create package.json in repository root
-  await outputFile('./package.json', pkg);
+  await outputJson('./package.json', {version: '0.0.0-dev'});
   // Create a npm-shrinkwrap.json file
   await execa('npm', ['shrinkwrap']);
-  const shrinkwrap = (await readFile('./npm-shrinkwrap.json')).toString();
 
   await updatePackageVersion('1.0.0', '.', t.context.logger);
 
   // Verify package.json and npm-shrinkwrap.json have been updated
-  t.is((await readFile('./package.json')).toString(), pkg.replace('0.0.0-dev', '1.0.0'));
-  t.is((await readFile('./npm-shrinkwrap.json')).toString(), shrinkwrap.replace('0.0.0-dev', '1.0.0'));
+  t.is((await readJson('./package.json')).version, '1.0.0');
+  t.is((await readJson('./npm-shrinkwrap.json')).version, '1.0.0');
   // Verify the logger has been called with the version updated
   t.deepEqual(t.context.log.args[0], ['Wrote version %s to %s', '1.0.0', 'package.json']);
   t.deepEqual(t.context.log.args[1], ['Wrote version %s to %s', '1.0.0', 'npm-shrinkwrap.json']);
 });
 
-test.serial('Updade package.json and npm-shrinkwrap.json in a sub-directory', async t => {
-  const pkg = `{
-    "name": "test","description":"pacakage description","version":"0.0.0-dev"
-  }`;
 
+test.serial('Updade package.json and npm-shrinkwrap.json in a sub-directory', async t => {
   // Create package.json in repository root
-  await outputFile('./dist/package.json', pkg);
+  await outputJson('./dist/package.json', {version: '0.0.0-dev'});
   process.chdir('dist');
   // Create a npm-shrinkwrap.json file
   await execa('npm', ['shrinkwrap']);
   process.chdir('..');
-  const shrinkwrap = (await readFile('./dist/npm-shrinkwrap.json')).toString();
 
   await updatePackageVersion('1.0.0', 'dist', t.context.logger);
 
   // Verify package.json and npm-shrinkwrap.json have been updated
-  t.is((await readFile('./dist/package.json')).toString(), pkg.replace('0.0.0-dev', '1.0.0'));
-  t.is((await readFile('./dist/npm-shrinkwrap.json')).toString(), shrinkwrap.replace('0.0.0-dev', '1.0.0'));
+  t.is((await readJson('./dist/package.json')).version, '1.0.0');
+  t.is((await readJson('./dist/npm-shrinkwrap.json')).version, '1.0.0');
   // Verify the logger has been called with the version updated
   t.deepEqual(t.context.log.args[0], ['Wrote version %s to %s', '1.0.0', 'dist/package.json']);
   t.deepEqual(t.context.log.args[1], ['Wrote version %s to %s', '1.0.0', 'dist/npm-shrinkwrap.json']);
