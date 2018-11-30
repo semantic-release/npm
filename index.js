642932b64016d48ea0f1564bb135c6c75209cdb6
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-11-29 21:12:46 -0500

    refactor: remove unnecessary `let` declaration

--- index.js
+++ index.js
@@ -43,14 +43,13 @@ async function verifyConditions(pluginConfig, context) {
 }
 
 async function prepare(pluginConfig, context) {
-  let pkg;
   const errors = verified ? [] : verifyNpmConfig(pluginConfig);
 
   setLegacyToken(context);
 
   try {
     // Reload package.json in case a previous external step updated it
-    pkg = await getPkg(pluginConfig, context);
+    const pkg = await getPkg(pluginConfig, context);
     if (!verified && pluginConfig.npmPublish !== false && pkg.private !== true) {
       await verifyNpmAuth(pluginConfig, pkg, context);
     }
