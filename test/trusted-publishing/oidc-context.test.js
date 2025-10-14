00ab5fcb9c2bca8fdb9c66189747a427dc4fdcff
Matt Travi <programmer@travi.org>
2025-10-14 11:42:30 -0500

    wip(trusted-publishing): limit successful oidc context detection to supported ci providers
    for #958

--- test/trusted-publishing/oidc-context.test.js
+++ test/trusted-publishing/oidc-context.test.js
@@ -1,10 +1,30 @@
 import test from "ava";
+import * as td from "testdouble";
 
 import { OFFICIAL_REGISTRY } from "../../lib/definitions/constants.js";
-import oidcContextEstablished from "../../lib/trusted-publishing/oidc-context.js";
+
+let oidcContextEstablished, trustedCiProvider;
+
+test.beforeEach(async (t) => {
+  ({ default: trustedCiProvider } = await td.replaceEsm("../../lib/trusted-publishing/supported-ci-provider.js"));
+
+  ({ default: oidcContextEstablished } = await import("../../lib/trusted-publishing/oidc-context.js"));
+});
+
+test.afterEach.always((t) => {
+  td.reset();
+});
 
 test("that `true` is returned when a trusted-publishing context has been established with the official registry", async (t) => {
+  td.when(trustedCiProvider()).thenResolve(true);
+
   t.true(await oidcContextEstablished(OFFICIAL_REGISTRY));
+});
+
+test("that `false` is returned when the official registry is targeted, but outside the context of a supported CI provider", async (t) => {
+  td.when(trustedCiProvider()).thenResolve(false);
+
+  t.false(await oidcContextEstablished(OFFICIAL_REGISTRY));
 });
 
 test("that `false` is returned when a custom registry is targeted", async (t) => {
