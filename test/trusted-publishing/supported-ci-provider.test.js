1e279d8bddc53ac698a8e1b17c9e1c4f308cf5e7
Matt Travi <programmer@travi.org>
2025-10-14 10:55:00 -0500

    wip(trusted-publishing): detect whether running from a trusted provider
    for #958

---
+++ test/trusted-publishing/supported-ci-provider.test.js
@@ -0,0 +1,34 @@
+import test from "ava";
+import * as td from "testdouble";
+
+//https://docs.npmjs.com/trusted-publishers#supported-cicd-providers
+
+let envCi, trustedCiProvider;
+
+test.beforeEach(async (t) => {
+  ({ default: envCi } = await td.replaceEsm("env-ci"));
+
+  ({ default: trustedCiProvider } = await import("../../lib/trusted-publishing/supported-ci-provider.js"));
+});
+
+test.afterEach.always((t) => {
+  td.reset();
+});
+
+test("that `true` is returned when GitHub Actions is detected", async (t) => {
+  td.when(envCi()).thenReturn({ name: "GitHub Actions" });
+
+  t.true(await trustedCiProvider());
+});
+
+test("that `true` is returned when GitLab Pipelines is detected", async (t) => {
+  td.when(envCi()).thenReturn({ name: "GitLab CI/CD" });
+
+  t.true(await trustedCiProvider());
+});
+
+test("that `false` is returned no supported CI provider is detected", async (t) => {
+  td.when(envCi()).thenReturn({ name: "Other Service" });
+
+  t.false(await trustedCiProvider());
+});
