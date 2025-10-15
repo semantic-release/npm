d83b727440121f4e029d5c2885e9cb55f8aab839
Matt Travi <programmer@travi.org>
2025-10-15 11:29:01 -0500

    feat(trusted-publishing): pass id-token as bearer header for github actions
    for #958

--- test/trusted-publishing/token-exchange.test.js
+++ test/trusted-publishing/token-exchange.test.js
@@ -1,16 +1,21 @@
 import test from "ava";
 import * as td from "testdouble";
 
-import tokenExchange from "../../lib/trusted-publishing/token-exchange.js";
 import { OFFICIAL_REGISTRY } from "../../lib/definitions/constants.js";
 
 // https://api-docs.npmjs.com/#tag/registry.npmjs.org/operation/exchangeOidcToken
 
+let tokenExchange, getIDToken;
 const packageName = "@scope/some-package";
 const pkg = { name: packageName };
+const idToken = "id-token-value";
+const token = "token-value";
 
 test.beforeEach(async (t) => {
   await td.replace(globalThis, "fetch");
+  ({ getIDToken } = await td.replaceEsm("@actions/core"));
+
+  ({ default: tokenExchange } = await import("../../lib/trusted-publishing/token-exchange.js"));
 });
 
 test.afterEach.always((t) => {
@@ -18,10 +23,11 @@ test.afterEach.always((t) => {
 });
 
 test.serial("that an access token is returned when token exchange succeeds", async (t) => {
-  const token = "token-value";
+  td.when(getIDToken("npm:registry.npmjs.org")).thenResolve(idToken);
   td.when(
     fetch(`${OFFICIAL_REGISTRY}-/npm/v1/oidc/token/exchange/package/${encodeURIComponent(packageName)}`, {
       method: "POST",
+      headers: { Authorization: `Bearer ${idToken}` },
     })
   ).thenResolve(
     new Response(JSON.stringify({ token }), { status: 201, headers: { "Content-Type": "application/json" } })
@@ -31,9 +37,11 @@ test.serial("that an access token is returned when token exchange succeeds", asy
 });
 
 test.serial("that `undefined` is returned when token exchange fails", async (t) => {
+  td.when(getIDToken("npm:registry.npmjs.org")).thenResolve(idToken);
   td.when(
     fetch(`${OFFICIAL_REGISTRY}-/npm/v1/oidc/token/exchange/package/${encodeURIComponent(packageName)}`, {
       method: "POST",
+      headers: { Authorization: `Bearer ${idToken}` },
     })
   ).thenResolve(
     new Response(JSON.stringify({ message: "foo" }), { status: 401, headers: { "Content-Type": "application/json" } })
