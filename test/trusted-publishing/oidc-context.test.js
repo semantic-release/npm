a4a5add415ab4ebdcddc78b0a8bedf4e66ec72f0
Matt Travi <programmer@travi.org>
2025-10-15 14:21:39 -0500

    feat(trusted-publishing): log progression of token-exchange steps
    for #958

--- test/trusted-publishing/oidc-context.test.js
+++ test/trusted-publishing/oidc-context.test.js
@@ -5,11 +5,12 @@ import { OFFICIAL_REGISTRY } from "../../lib/definitions/constants.js";
 
 let oidcContextEstablished, trustedCiProvider, exchangeToken;
 const pkg = {};
+const context = {};
 
 test.beforeEach(async (t) => {
   await td.replace(globalThis, "fetch");
   ({ default: exchangeToken } = await td.replaceEsm("../../lib/trusted-publishing/token-exchange.js"));
-  td.when(exchangeToken(pkg)).thenResolve(undefined);
+  td.when(exchangeToken(pkg, context)).thenResolve(undefined);
 
   ({ default: oidcContextEstablished } = await import("../../lib/trusted-publishing/oidc-context.js"));
 });
@@ -22,19 +23,19 @@ test.serial(
   "that `true` is returned when a trusted-publishing context has been established with the official registry",
   async (t) => {
     td.when(fetch("https://matt.travi.org")).thenResolve(new Response(null, { status: 401 }));
-    td.when(exchangeToken(pkg)).thenResolve("token-value");
+    td.when(exchangeToken(pkg, context)).thenResolve("token-value");
 
-    t.true(await oidcContextEstablished(OFFICIAL_REGISTRY, pkg));
+    t.true(await oidcContextEstablished(OFFICIAL_REGISTRY, pkg, context));
   }
 );
 
 test.serial("that `false` is returned when OIDC token exchange fails in a supported CI provider", async (t) => {
   td.when(fetch("https://matt.travi.org")).thenResolve(new Response(null, { status: 401 }));
-  td.when(exchangeToken(pkg)).thenResolve(undefined);
+  td.when(exchangeToken(pkg, context)).thenResolve(undefined);
 
-  t.false(await oidcContextEstablished(OFFICIAL_REGISTRY, pkg));
+  t.false(await oidcContextEstablished(OFFICIAL_REGISTRY, pkg, context));
 });
 
 test.serial("that `false` is returned when a custom registry is targeted", async (t) => {
-  t.false(await oidcContextEstablished("https://custom.registry.org/", pkg));
+  t.false(await oidcContextEstablished("https://custom.registry.org/", pkg, context));
 });
